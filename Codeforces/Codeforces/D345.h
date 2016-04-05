ee86fe3d468c2316afff89e1f80f58ac80299c9d
Jie <sun.tzu.0909@gmail.com>
2016-04-04 20:12:38 -0400

    Codeforce Div 2. Round #345. Problem D.

---
+++ Codeforces/Codeforces/D345.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+
+class D345
+{
+public:
+    D345();
+    int D345::MaxNumberOfPhotos(int numberOfPhotos, int timeToMoveFromPhotoToAdj, long timeToChangeOrientation, std::string photoOrientations);
+    ~D345();
+};
+
